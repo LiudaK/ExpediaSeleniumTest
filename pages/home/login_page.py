b0051561a355432f0397b6acbfe3313919f47653
Liudmila Karabelnikava <karabelnikava@gmail.com>
2018-10-23 22:03:37 -0700

    added methods in login_page

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -3,17 +3,46 @@ from selenium.webdriver.common.by import By
 class LoginPage():
     def __init__(self,driver):
         self.driver = driver
+    #Locators
+    _account_link = "header-account-menu"
+    _login_link = "account-signin"
+    _email_field = "gss-signin-email"
+    _password_field = "gss-signin-password"
+    _login_button = "gss-signin-submit"
 
-    def login(self, username, password):
-        account = self.driver.find_element(By.ID, "header-account-menu")
-        account.click()
-        login = self.driver.find_element(By.ID, "account-signin")
-        login.click()
+    def getAccountLink(self):
+        return self.driver.find_element(By.ID, self._account_link)
 
-        usernameLink = self.driver.find_element(By.ID, "gss-signin-email")
-        usernameLink.send_keys(username)
-        passwordLink = self.driver.find_element(By.ID, "gss-signin-password")
-        passwordLink.send_keys(password)
+    def getLoginLink(self):
+        return self.driver.find_element(By.ID, self._login_link)
 
-        submitbutton = self.driver.find_element(By.ID, "gss-signin-submit")
-        submitbutton.click()
+    def getEmailLink(self):
+        return self.driver.find_element(By.ID, self._email_field)
+
+    def getPasswordLink(self):
+        return self.driver.find_element(By.ID, self._password_field)
+
+    def getLoginButton(self):
+        return self.driver.find_element(By.ID, self._login_button)
+
+    def clickAccountLink(self):
+        self.getAccountLink().click()
+
+    def clickLoginLink(self):
+        self.getLoginLink().click()
+
+    def enterEmail(self, email):
+        self.getEmailLink().send_keys(email)
+
+    def enterPassword(self, password):
+        self.getPasswordLink().send_keys(password)
+
+    def clickLoginButton(self):
+        self.getLoginButton().click()
+
+    def login(self, email, password):
+        self.clickAccountLink()
+        self.clickLoginLink()
+        self.enterEmail(email)
+        self.enterPassword(password)
+        self.clickLoginButton()
