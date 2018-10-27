c62d98991c45a313424844b30c99de5083f31010
Liudmila Karabelnikava <karabelnikava@gmail.com>
2018-10-27 12:28:23 -0700

    added conftest

--- tests/home/login_test.py
+++ tests/home/login_test.py
@@ -5,6 +5,7 @@ Windows:
 http://stackoverflow.com/questions/3701646/how-to-add-to-the-pythonpath-in-windows-7
 Mac:
 http://stackoverflow.com/questions/3387695/add-to-python-path-mac-os-x
+http://pytest-ordering.readthedocs.io/en/develop/
 """
 from selenium import webdriver
 from pages.home.login_page import LoginPage
@@ -13,30 +14,28 @@ import utilities.custom_logger as cl
 import logging
 import pytest
 
+
+@pytest.mark.usefixtures("oneTimeSetUp", "setUp")
 class LoginTest(unittest.TestCase):
 
     log = cl.customLogger(logging.DEBUG)
-    baseURL = "https://www.expedia.com/"
-    driver = webdriver.Chrome()
-    # driver.maximize_window()
-    driver.implicitly_wait(0.5)
-    lp = LoginPage(driver)
 
-    # @pytest.mark.run(order=1)
-    # def test_invalidLogin(self):
-    #     self.driver.get(self.baseURL)
-    #     self.lp.login("1", "1")
-    #     result = self.lp.verifyLoginFail()
-    #     assert result == True
-    #     self.driver.quit()
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.lp = LoginPage(self.driver)
 
-    #@pytest.mark.run(order=2)
+    @pytest.mark.run(order=2)
     def test_validLogin(self):
-        self.driver.get(self.baseURL)
         self.lp.login("luda4utest@gmail.com", "passwordabc")
         result = self.lp.verifyLoginSuccessful()
         assert result == True
-        self.driver.quit()
+
+    @pytest.mark.run(order=1)
+    def test_invalidLogin(self):
+        self.lp.login("1", "1")
+        result = self.lp.verifyLoginFail()
+        assert result == True
+
 
 
 
